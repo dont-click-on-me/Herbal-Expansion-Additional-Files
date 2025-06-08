33b21326b873838a91714397f7ebcd1ed5126c7f
dont-click-on-me <oj.vvtt@gmail.com>
2025-06-08 15:56:40 -0400

    KubeJS client script

---
+++ client_scripts/herbal_expansion_modifications.js
@@ -0,0 +1,88 @@
+ClientEvents.lang('en_us', event => {
+  // Black Mamba
+  event.renameItem("herbalexpansion:fresh_black_mamba_nug", "§8Fresh Black Mamba Nug")
+  event.renameItem("herbalexpansion:dried_black_mamba_nug", "§8Dried Black Mamba Nug")
+  event.renameItem("herbalexpansion:black_mamba_oil", "§8Black Mamba Oil")
+  event.renameItem("herbalexpansion:black_mamba_joint", "§8Black Mamba Joint")
+
+  // Blue Dream
+  event.renameItem("herbalexpansion:fresh_blue_dream_nug", "§bFresh Blue Dream Nug")
+  event.renameItem("herbalexpansion:dried_blue_dream_nug", "§bDried Blue Dream Nug")
+  event.renameItem("herbalexpansion:blue_dream_oil", "§bBlue Dream Oil")
+  event.renameItem("herbalexpansion:blue_dream_joint", "§bBlue Dream Joint")
+
+  // Girl Scout Cookies
+  event.renameItem("herbalexpansion:fresh_girl_scout_cookies_nug", "§dFresh Girl Scout Cookies Nug")
+  event.renameItem("herbalexpansion:dried_girl_scout_cookies_nug", "§dDried Girl Scout Cookies Nug")
+  event.renameItem("herbalexpansion:girl_scout_cookies_oil", "§dGirl Scout Cookies Oil")
+  event.renameItem("herbalexpansion:girl_scout_cookies_joint", "§dGirl Scout Cookies Joint")
+
+  // Lemon Haze
+  event.renameItem("herbalexpansion:fresh_lemon_haze_nug", "§eFresh Lemon Haze Nug")
+  event.renameItem("herbalexpansion:dried_lemon_haze_nug", "§eDried Lemon Haze Nug")
+  event.renameItem("herbalexpansion:lemon_haze_oil", "§eLemon Haze Oil")
+  event.renameItem("herbalexpansion:lemon_haze_joint", "§eLemon Haze Joint")
+
+  // Northern Lights
+  event.renameItem("herbalexpansion:fresh_northern_lights_nug", "§2Fresh Northern Lights Nug")
+  event.renameItem("herbalexpansion:dried_northern_lights_nug", "§2Dried Northern Lights Nug")
+  event.renameItem("herbalexpansion:northern_lights_oil", "§2Northern Lights Oil")
+  event.renameItem("herbalexpansion:northern_lights_joint", "§2Northern Lights Joint")
+
+  // Panama Red
+  event.renameItem("herbalexpansion:fresh_panama_red_nug", "§cFresh Panama Red Nug")
+  event.renameItem("herbalexpansion:dried_panama_red_nug", "§cDried Panama Red Nug")
+  event.renameItem("herbalexpansion:panama_red_oil", "§cPanama Red Oil")
+  event.renameItem("herbalexpansion:panama_red_joint", "§cPanama Red Joint")
+
+  // Purple Haze
+  event.renameItem("herbalexpansion:fresh_purple_haze_nug", "§5Fresh Purple Haze Nug")
+  event.renameItem("herbalexpansion:dried_purple_haze_nug", "§5Dried Purple Haze Nug")
+  event.renameItem("herbalexpansion:purple_haze_oil", "§5Purple Haze Oil")
+  event.renameItem("herbalexpansion:purple_haze_joint", "§5Purple Haze Joint")
+
+  // Sour Diesel
+  event.renameItem("herbalexpansion:fresh_sour_diesel_nug", "§6Fresh Sour Diesel Nug")
+  event.renameItem("herbalexpansion:dried_sour_diesel_nug", "§6Dried Sour Diesel Nug")
+  event.renameItem("herbalexpansion:sour_diesel_oil", "§6Sour Diesel Oil")
+  event.renameItem("herbalexpansion:sour_diesel_joint", "§6Sour Diesel Joint")
+
+  // White Widow
+  event.renameItem("herbalexpansion:fresh_white_widow_nug", "§fFresh White Widow Nug")
+  event.renameItem("herbalexpansion:dried_white_widow_nug", "§fDried White Widow Nug")
+  event.renameItem("herbalexpansion:white_widow_oil", "§fWhite Widow Oil")
+  event.renameItem("herbalexpansion:white_widow_joint", "§fWhite Widow Joint")
+
+  // Cannabis Leaf - Lime Green
+  event.renameItem("herbalexpansion:cannabis_leaf", "§aCannabis Leaf")
+})
+
+ItemEvents.modifyTooltips(event => {
+  event.add("herbalexpansion:blue_dream_joint", [
+    Text.of("Effect: Water Breathing").gray()
+  ])
+  event.add("herbalexpansion:girl_scout_cookies_joint", [
+    Text.of("Effect: Regeneration").gray()
+  ])
+  event.add("herbalexpansion:lemon_haze_joint", [
+    Text.of("Effect: Haste").gray()
+  ])
+  event.add("herbalexpansion:northern_lights_joint", [
+    Text.of("Effect: Speed").gray()
+  ])
+  event.add("herbalexpansion:panama_red_joint", [
+    Text.of("Effect: Strength").gray()
+  ])
+  event.add("herbalexpansion:purple_haze_joint", [
+    Text.of("Effect: Levitation").gray()
+  ])
+  event.add("herbalexpansion:sour_diesel_joint", [
+    Text.of("Effect: Fire Resistance").gray()
+  ])
+  event.add("herbalexpansion:white_widow_joint", [
+    Text.of("Effect: Night Vision").gray()
+  ])
+  event.add("herbalexpansion:black_mamba_joint", [
+    Text.of("Effect: Invisibility").gray()
+  ])
+})
